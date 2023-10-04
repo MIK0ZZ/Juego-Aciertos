ab62180d67c1a20461fe3984b7b90513186617da
BelZ <kbelz21z@gmail.com>
2023-10-04 20:19:55 +0200

    Cambio nombre main class

--- src/Juego.java
+++ src/Juego.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class App {
+public class Juego {
     public static void main(String[] args) throws Exception {
 
         /*  Para borrar la consola
